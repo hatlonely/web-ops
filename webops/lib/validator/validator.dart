b2f905214445e1b05777337f989793898a3978c6
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-21 00:13:46 +0800

    update

--- webops/lib/validator/validator.dart
+++ webops/lib/validator/validator.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 class StringValidator {
-  static String Function(String) group(List<String Function(String)> validators) {
+  static String? Function(String)? group(List<String? Function(String)> validators) {
     return (String value) {
       for (var validator in validators) {
         var v = validator(value);
@@ -13,14 +13,14 @@ class StringValidator {
     };
   }
 
-  static String required(String value) {
-    if ((value?.isEmpty ?? true) || value.trim().isEmpty) {
+  static String? required(String? value) {
+    if ((value?.isEmpty ?? true) || value!.trim().isEmpty) {
       return "不能为空";
     }
     return null;
   }
 
-  static String isJson(String value) {
+  static String? isJson(String? value) {
     if (value == null) {
       return "非法的 json";
     }
